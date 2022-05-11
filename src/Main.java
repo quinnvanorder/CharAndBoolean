4b6b3f85a4972043f7effd8d54a11f8baf047c47
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-10 19:49:30 -0700

    The char and boolean Primitive Data Types

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //2 bits, can hold unicode
+        char myChar = 'D';
+        char myUnicode = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBool = true;
+        boolean myFalseBool = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+
+    }
+}
